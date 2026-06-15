aad8e08d33cffd43e85af1b644c2603eb8db1b13
Rahul Pagare <165076212+RahulPagare20@users.noreply.github.com>
2026-06-15 14:21:28 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,6 +1,6 @@
 import smtplib
 
-email = "connitdevs@gmail.com"
+email = "" # You might want to use your own email address
 
 
 def send_email(receiver, subject, message):
@@ -9,7 +9,7 @@ def send_email(receiver, subject, message):
         text = f"Subject: {subject}\n\n{message}"
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login(email, "zeibftsoamrlpgln")
+        server.login(email, "") # Put password in double quotations
         server.sendmail(email, receiver, text)
         return True
     except:
